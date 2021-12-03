67839d1fb6698ecd6acd30f9e1c53fd43c44e177
LAPTOP-56NU4RUS\jenni <jennifer7793@gmail.com>
2021-12-03 16:58:01 +0800

    task_3_complete

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -2,22 +2,33 @@
 document.addEventListener("DOMContentLoaded", ()=> {
   const closeBtn = document.querySelector(".close");
   const ul = document.querySelector("ul")
-  const list = document.querySelectorAll("li")
-  
-  list.forEach(li => li.addEventListener("click", () => {
-    if (li.className !== "checked") {
-    li.className = "checked"
-    }
-  }))
 
   ul.addEventListener("click", (e) => {
     if (e.target.nodeName === "SPAN") {
     e.target.parentNode.remove()
     }
   })
+
+  const input = document.querySelector("#input")
+  const addBtn = document.querySelector("#addBtn")
+  addBtn.addEventListener("click", () => {
+    inputValue = input.value.trim()
+    if (inputValue !== "") {
+    const todoList = document.createElement("li")
+    const deleteBtn = document.createElement("span")
+    todoList.innerText = input.value
+    deleteBtn.innerText = "X"
+    deleteBtn.className = "close" 
+    ul.append(todoList)
+    todoList.append(deleteBtn)
+   
+    const list = document.querySelectorAll("li")
+    list.forEach(li => li.addEventListener("click", () => {
+      if (li.className !== "checked") {
+      li.className = "checked"
+      }
+    }))
+    }
+  })
 });
 
-// closeBtn.addEventListener("click", (e) => {
-//   console.log(e);
-//   // e.target.parentNode.remove();
-// })
